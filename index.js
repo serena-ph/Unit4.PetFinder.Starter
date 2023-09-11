6f8ab69ef0cb01684abe86865cf9a101e585e21f
SerenaTPhan <pttnhann@gmail.com>
2023-09-11 13:19:29 -0500

    finish

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const PORT = 8080;
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+
 
 });
 
@@ -20,31 +22,44 @@ app.get('/api', (req, res) => {
 
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
+  
     // send the pets array as a response
+
+    res.send(pets);
 
 });
 
 // get pet by owner with query string
-app.get('/api/v1/pets/owner', (req, res) => {
+app.get('/api/v1/pets/owner/:owner', (req, res) => {
+    
+    
     // get the owner from the request
+       const { owner } = req.params;
 
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
+    console.log(pet)
 
     // send the pet as a response
+      res.send(pet)
+    
 
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
+    const { name } = req.params;
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
+    
 
     // send the pet as a response
+    res.send(pet);
+
+    
 
 });
 
